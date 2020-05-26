6fe17b44c4ee95c425def88c41ae6ec2942b4f30
zhanzghiyong0410 <2556703570@qq.com>
2020-05-26 18:42:00 +0800

    update

--- tetris.py
+++ tetris.py
@@ -37,66 +37,14 @@ class Coordinate:
         self.clo = clo
         self.color = color
 
-txT=[
 
-    Coordinate(0,0,colortian),
-    Coordinate(0,1,colortian),
-    Coordinate(1,0,colortian),
-    Coordinate(1,1,colortian)
-    
-]
 
-txSd=[
 
-    Coordinate(0,0,colorshandian),
-    Coordinate(0,1,colorshandian),
-    Coordinate(1,1,colorshandian),
-    Coordinate(1,2,colorshandian)
-]
 
-txFsd=[
 
-    Coordinate(1,0,colorfshandian),
-    Coordinate(1,1,colorfshandian),
-    Coordinate(0,1,colorfshandian),
-    Coordinate(0,2,colorfshandian)
-]
 
-txGz=[
 
-    Coordinate(0,0,colorgouzi),
-    Coordinate(0,1,colorgouzi),
-    Coordinate(1,1,colorgouzi),
-    Coordinate(2,1,colorgouzi)
 
-]
-
-txFgz=[
-
-    Coordinate(0,0,colorfgouzi),
-    Coordinate(0,1,colorfgouzi),
-    Coordinate(1,0,colorfgouzi),
-    Coordinate(2,0,colorfgouzi)
-    
-]
-
-txBd=[
-
-    Coordinate(0,0,colorbiandan),
-    Coordinate(1,0,colorbiandan),
-    Coordinate(2,0,colorbiandan),
-    Coordinate(3,0,colorbiandan)
-    
-]
-
-txFj=[
-
-    Coordinate(0,1,colorfeiji),
-    Coordinate(1,0,colorfeiji),
-    Coordinate(1,1,colorfeiji),
-    Coordinate(1,2,colorfeiji)
-    
-]
 
 def huizhi(color,left,top):
     lefts = windowsWidth / clo * left
@@ -110,19 +58,59 @@ def suijituxing(randowGraphics):
     sjtx = []
 
     if randowGraphics == 0:
-        sjtx = txT
+        sjtx=[
+            Coordinate(0,0,colortian),
+            Coordinate(0,1,colortian),
+            Coordinate(1,0,colortian),
+            Coordinate(1,1,colortian)
+        ]
     elif randowGraphics == 1:
-        sjtx = txSd
+        sjtx=[
+            Coordinate(1,0,colorfshandian),
+            Coordinate(1,1,colorfshandian),
+            Coordinate(0,1,colorfshandian),
+            Coordinate(0,2,colorfshandian)
+        ]
     elif randowGraphics == 2:
-        sjtx = txFsd
+        sjtx=[
+            Coordinate(1,0,colorfshandian),
+            Coordinate(1,1,colorfshandian),
+            Coordinate(0,1,colorfshandian),
+            Coordinate(0,2,colorfshandian)
+        ]
+
     elif randowGraphics == 3:
-        sjtx = txGz
+        sjtx=[
+            Coordinate(0,0,colorgouzi),
+            Coordinate(0,1,colorgouzi),
+            Coordinate(1,1,colorgouzi),
+            Coordinate(2,1,colorgouzi)
+
+]
     elif randowGraphics == 4:
-        sjtx = txFgz
+        sjtx=[
+            Coordinate(0,0,colorfgouzi),
+            Coordinate(0,1,colorfgouzi),
+            Coordinate(1,0,colorfgouzi),
+            Coordinate(2,0,colorfgouzi)
+            
+        ]
     elif randowGraphics == 5:
-        sjtx = txBd
+        sjtx=[
+            Coordinate(0,0,colorbiandan),
+            Coordinate(1,0,colorbiandan),
+            Coordinate(2,0,colorbiandan),
+            Coordinate(3,0,colorbiandan)
+            
+        ]
     elif randowGraphics == 6:
-        sjtx = txFj
+        sjtx=[
+            Coordinate(0,1,colorfeiji),
+            Coordinate(1,0,colorfeiji),
+            Coordinate(1,1,colorfeiji),
+            Coordinate(1,2,colorfeiji)
+            
+        ]
 
     return sjtx
 
@@ -178,7 +166,10 @@ while True:
                     sjtx[hz].row -= 1
                     hz -= 1
                 stop = False
-                xz = True
+                
+                for pc in sjtx:
+                    pinchou.append(pc)
+
                 break
     else:
         randowGraphics = random.randint(0,6)
@@ -186,11 +177,6 @@ while True:
         randomNumber = suijiweizhi(randowGraphics)
 
         stop = True
-        xz = False
-
-    if xz:
-        for pc in sjtx:
-            pinchou.append(pc)
     
     for pchz in pinchou:
         huizhi(pchz.color,pchz.clo,pchz.row)
